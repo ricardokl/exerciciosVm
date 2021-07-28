38e842b36f347ccf6a20cf7bf4a2558e68de0934
Ricardo Kullock <ricardokullock@gmail.com>
2021-07-28 15:13:38 -0300

    String docs e type hints

--- randVm.py
+++ randVm.py
@@ -2,37 +2,44 @@ import random
 import datetime
 from pathlib import Path
 
-#função que retorna as variáveis necessárias
-def rand():
+def rand(vmax: int = 100, xmax: int = 50000) -> list[float]:
+    """
+    Função retorna as variáveis aleatórias necessárias
+    """
     # Velocidade aleatória
-    vm = random.randint(-100, 100)
+    vm = random.randint(-vmax, vmax)
     # Gerando uma lista de 2 tempos diferentes (função "sample"), escolhidos
     # de uma lista de inteiros entre 0:00 e 24:00, intervalos de 15 min
     t = sorted(random.sample([i/100 for i in range(0,2300,25)], 2))
     # Inicializando uma lista de posições
     x = list()
     # Primeira posição da lista gerada de forma aleatória
-    x.append(random.randrange(-50000, 50000,25)/100)
+    x.append(random.randrange(-xmax, xmax,25)/100)
     # Segunda posição da lista criada de forma a fechar com t e vm
     x.append(vm*(t[1]-t[0]) + x[0])
     # return vm, th, tm, t, x
     return vm, t, x
 
 
-# Transformando tudo em strings
-def str_vars():
-    # Definindo as variáveis globalmente
-    vm, t, x = rand()
+def str_vars(vmax: int, xmax: int) -> list[str]:
+    """
+    Transformando tudo em strings para usar nos temaplates
+    """
+    # Chamando a função principal
+    vm, t, x = rand(vmax, xmax)
     x_string = [str(a).replace('.',',') for a in x]
     t_string = [str(datetime.timedelta(hours=a))[:-3] for a in t]
     deltax = [str(x[1]-x[0]).replace('.',',')]
     deltat = [str(datetime.timedelta(hours=t[1]-t[0]))[:-3]]
     # Objeto do movimento pode ser ônibus ou carro de passeio
-    obj = [random.choice(['ônibus', 'carro de passeio'])]
+    obj = [random.choice(['ônibus', 'carro de passeio', "caminhão"])]
+    # Objeto de retorno é uma concatenação de listas
     return obj + x_string + t_string + deltax + deltat
 
-# Função final
-def exercicio():
+def exercicio() -> str:
+    """
+    Função geradora de exercícios aleatórios usando templates em ./templates
+    """
     # Variáveis definidas no tamplate
     nomes = ['{obj}', '{x0}', '{x1}', '{t0}', '{t1}', '{deltax}', '{deltat}']
     # Dicionário com as substituições
@@ -46,6 +53,5 @@ def exercicio():
         escolha = escolha.replace(i[0],i[1])
     return escolha
 
-# Chamando a função final
 if __name__ ==  '__main__':
     print(exercicio())
