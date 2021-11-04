2518771bbffa4d2fb89477dbeb5f8481893f026e
gomesjpeg <joaopedrogom.rocha@gmail.com>
2021-11-04 14:16:00 -0300

    commit

--- randVm.py
+++ randVm.py
@@ -1,57 +1,19 @@
-import random
-import datetime
-from pathlib import Path
+import random as rd
+import datetime 
 
-def rand(vmax = 100, xmax  = 50000):
-    """
-    Função retorna as variáveis aleatórias necessárias
-    """
-    # Velocidade aleatória
-    vm = random.randint(-vmax, vmax)
-    # Gerando uma lista de 2 tempos diferentes (função "sample"), escolhidos
-    # de uma lista de inteiros entre 0:00 e 24:00, intervalos de 15 min
-    t = sorted(random.sample([i/100 for i in range(0,2300,25)], 2))
-    # Inicializando uma lista de posições
-    x = list()
-    # Primeira posição da lista gerada de forma aleatória
-    x.append(random.randrange(-xmax, xmax,25)/100)
-    # Segunda posição da lista criada de forma a fechar com t e vm
-    x.append(vm*(t[1]-t[0]) + x[0])
-    # return vm, th, tm, t, x
-    return vm, t, x
+#criando lista de transporte
+t = rd.choice(["carro" , "ônibus"])
+#criando função das varivaveis
+def variaveis(vmin = 20 , vmax = 100):
+    vm= rd.randint(vmin, vmax)
+    tempo = rd.randint(0,55)
+    d = vm*tempo
+    hinicial = str(datetime.timedelta())
+    hfinal = str(datetime.timedelta())
+    return vm, d, hinicial, hfinal
 
-def str_vars():
-    """
-    Transformando tudo em strings para usar nos temaplates
-    """
-    # Chamando a função principal
-    _, t, x = rand()
-    x_string = [str(a).replace('.',',') for a in x]
-    # [:-3] retira os segundos da string
-    t_string = [str(datetime.timedelta(hours=a))[:-3] for a in t]
-    deltax = [str(x[1]-x[0]).replace('.',',')]
-    deltat = [str(datetime.timedelta(hours=t[1]-t[0]))[:-3]]
-    # Objeto do movimento pode ser ônibus ou carro de passeio
-    obj = [random.choice(['ônibus', 'carro de passeio', 'caminhão'])]
-    # Objeto de retorno é uma concatenação de listas
-    return obj + x_string + t_string + deltax + deltat
 
-def exercicio():
-    """
-    Função geradora de exercícios aleatórios usando templates em ./templates
-    """
-    # Variáveis definidas no tamplate
-    nomes = ['{obj}', '{x0}', '{x1}', '{t0}', '{t1}', '{deltax}', '{deltat}']
-    # Dicionário com as substituições
-    subs = {nome:var for nome, var in zip(nomes,str_vars())}
-    # Lista com todos os caminhos dos arquivos da pasta /template
-    templates = [x for x in Path(__file__).parent.joinpath("templates").iterdir()]
-    # Escolhendo um arquivo aleatório e lendo o texto dele
-    escolha = random.choice(templates).read_text()
-    # Loop substituindo todos as variáveis no texto
-    for i in subs.items():
-        escolha = escolha.replace(i[0],i[1])
-    return escolha
-
-if __name__ ==  '__main__':
-    print(exercicio())
+#criando um exercicio para teste(perguntar ao ricardo oq fazer para conseguir utilizar o timedelta de maneira eficaz.)
+if t == "ônibus":
+    vm , d , hinicial , hfinal = variaveis()
+    print(f'um {t} saí do seu posto às {hinicial[0:5]} horas e chega na cidade às {hfinal[0:5]}, sabendo que em todo seu percurso, o {t} teve uma velocidade constante de {vm}km, calcule a distancia entre o posto e a cidade.')
